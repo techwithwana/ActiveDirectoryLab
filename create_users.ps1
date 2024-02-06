b14df1fe089cc11c8c328c50290c9a97ca7d32f2
Anwana Ntofon <158541556+techwithwana@users.noreply.github.com>
2024-02-05 22:00:47 -0800

    Added PowerShell Script

---
+++ create_users.ps1
@@ -0,0 +1,22 @@
+﻿$USERS_PASSWORD   = "Password1"
+$USER_FIRST_LAST_LIST = Get-Content .\names.txt
+
+$password = ConvertTo-SecureString $USERS_PASSWORD -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($item in $USER_FIRST_LAST_LIST) {
+    $firstName = $item.Split(" ")[0].ToLower()
+    $lastName = $item.Split(" ")[1].ToLower()
+    $username = "$($firstName.Substring(0,1))$($lastName)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Green
+    
+    New-AdUser -AccountPassword $password `
+               -GivenName $firstName `
+               -Surname $lastName `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
